2945ee4ac481f7bfe93c6a42107d6d9cee73bf72
Nelson Mario Flórez Jiménez <nflorezawh@gmail.com>
2024-01-31 15:58:57 +0000

    ?

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
 
     if(number < 0)
     {
-        perror("The number must be greater than or equal to zero\n");
-        return(1);
+        fprintf(stderr,"The number must be greater than or equal to zero\n");
+        retun(1);
     }
 
     /*Implement the function that calculates the factorial of the input argument*/
